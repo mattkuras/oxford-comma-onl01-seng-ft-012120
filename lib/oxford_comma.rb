2b4aeb09adc404038097dcbc65708cc5c0685298
Matt Kuras <mattkuras@gmail.com>
2020-01-25 19:35:35 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,12 @@
 def oxford_comma(array)
+<<<<<<< HEAD
  if array.length == 2 
    return array.join(" and ")
  elsif array.length >= 3
    return "#{array[0..-2].join(", ")}, and #{array.last}"
  else  return array.join
 end
+=======
+  return array.join(", ")
+>>>>>>> 68b276011e01eec0a90574cde3e66183f9034677
 end
